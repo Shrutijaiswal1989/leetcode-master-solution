48b1cbfa5445210586e2a3d48373a9681ffabba8
Shrutijaiswal1989 <153531904+Shrutijaiswal1989@users.noreply.github.com>
2024-04-21 20:56:12 +0530

    Time: 1774 ms (10.18%), Space: 273.9 MB (5.85%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,45 +1,30 @@
-class UnionFind {
- public:
-  UnionFind(int n) : id(n), rank(n) {
-    iota(id.begin(), id.end(), 0);
-  }
-
-  void unionByRank(int u, int v) {
-    const int i = find(u);
-    const int j = find(v);
-    if (i == j)
-      return;
-    if (rank[i] < rank[j]) {
-      id[i] = j;
-    } else if (rank[i] > rank[j]) {
-      id[j] = i;
-    } else {
-      id[i] = j;
-      ++rank[j];
-    }
-  }
-
-  int find(int u) {
-    return id[u] == u ? u : id[u] = find(id[u]);
-  }
-
- private:
-  vector<int> id;
-  vector<int> rank;
-};
-
 class Solution {
- public:
-  bool validPath(int n, vector<vector<int>>& edges, int source,
-                 int destination) {
-    UnionFind uf(n);
-
-    for (const vector<int>& edge : edges) {
-      const int u = edge[0];
-      const int v = edge[1];
-      uf.unionByRank(u, v);
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        unordered_map<int, vector<int>> graph;
+        for (const auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        
+        unordered_set<int> visited;
+        return dfs(source, destination, graph, visited);
     }
-
-    return uf.find(source) == uf.find(destination);
-  }
+    
+    bool dfs(int node, int destination, unordered_map<int, vector<int>>& graph, unordered_set<int>& visited) {
+        if (node == destination) {
+            return true;
+        }
+        visited.insert(node);
+        for (int neighbor : graph[node]) {
+            if (visited.find(neighbor) == visited.end()) {
+                if (dfs(neighbor, destination, graph, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 };
