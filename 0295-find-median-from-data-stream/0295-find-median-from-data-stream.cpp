4e1af0cb06b073ed42fa2cd3f7f7ed373c6f42bf
Shrutijaiswal1989 <153531904+Shrutijaiswal1989@users.noreply.github.com>
2024-05-01 13:51:11 +0530

    Time: 767 ms (5.01%), Space: 121.7 MB (82.8%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,26 @@
+class MedianFinder {
+private:
+    vector<float> arr;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(float num) {
+        // Insert the number into the sorted array
+        int i = 0;
+        while (i < arr.size() && num > arr[i]) {
+            i++;
+        }
+        arr.insert(arr.begin() + i, num);
+    }
+    
+    double findMedian() {
+        int n = arr.size();
+        if (n % 2 == 0) {
+            return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+        } else {
+            return arr[n / 2];
+        }        
+    }
+};
