ba74fbeeb3f93651b6307ac131d40d311df9e2ef
Shrutijaiswal1989 <153531904+Shrutijaiswal1989@users.noreply.github.com>
2024-11-24 10:10:19 +0530

    Time: 0 ms (100%), Space: 38.4 MB (30.74%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
- public:
-  long long maxMatrixSum(vector<vector<int>>& matrix) {
-    long long absSum = 0;
-    int minAbs = INT_MAX;
-    // 0 := even number of negatives
-    // 1 := odd number of negatives
-    int oddNeg = 0;
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int minValue = INT_MAX;
+        long long sum = 0;
+        int negCount = 0;
 
-    for (const vector<int>& row : matrix)
-      for (const int num : row) {
-        absSum += abs(num);
-        minAbs = min(minAbs, abs(num));
-        if (num < 0)
-          oddNeg ^= 1;
-      }
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] < 0)
+                    negCount++;
+                int absValue = abs(matrix[i][j]);
+                minValue = min(minValue, absValue);
+                sum += absValue;
+            }
+        }
 
-    return absSum - oddNeg * minAbs * 2;
-  }
+        if (negCount % 2 == 0)
+            return sum;
+        return sum - 2 * minValue;
+    }
 };
