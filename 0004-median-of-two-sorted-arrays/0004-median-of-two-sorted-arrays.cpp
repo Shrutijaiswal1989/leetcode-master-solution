2de33423d3e75e4f28c068bdf5fe57088b26075b
Shrutijaiswal1989 <153531904+Shrutijaiswal1989@users.noreply.github.com>
2024-11-26 21:28:25 +0530

    Time: 7 ms (13.08%), Space: 95.7 MB (26.9%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,114 +1,33 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // Get the sizes of both input arrays.
+        int n = nums1.size();
+        int m = nums2.size();
 
-int bs(int *n, int sz, int k) {
-    int i, j, m;
-    i = 0;
-    j = sz - 1;
-    while (i  <= j) {
-        m = i + (j - i) / 2;
-        if (n[m] > k) {
-            j = m - 1;
-        } else {
-            i = m + 1;
+        // Merge the arrays into a single sorted array.
+        vector<int> merged;
+        for (int i = 0; i < n; i++) {
+            merged.push_back(nums1[i]);
         }
-    }
-    return j;   // this is the lastest one which is not greater than k
-}
-double bs2(int *n1, int sz1, int *n2, int sz2, int m, int m1) {
-    double d;
-    int i;
-    i = bs(n1, sz1, n2[0]); // search in first array
-    if (i >= m) {           // median is in the first array
-        d = n1[m];
-        if (m1) {
-            if (i > m) {
-                d += n1[m + 1];
-            } else {
-                d += n2[0];
-            }
-            d /= 2;
+        for (int i = 0; i < m; i++) {
+            merged.push_back(nums2[i]);
         }
-    } else if (i == sz1 - 1) {  // median is in the second array
-        d = n2[m - i - 1];
-        if (m1) {
-            d += n2[m - i];
-            d /= 2;
-        }
-    } else {                    // reverse arrays and search again
-        d = bs2(n2, sz2, &n1[i + 1], sz1 - i - 1, m - i - 1, m1);
-    }
-    return d;
-}
-int min(int a, int b) {
-    return a  <  b ? a : b;
-}
-int max(int a, int b) {
-    return a > b ? a : b;
-}
-double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    double d;
-#if 0  // sliding & binary search
-    int i1_left = 0, i1_right = nums1Size;
-    int i1, i2;
-    
-    if (nums1Size > nums2Size) {  // make nums1 as a short array
-        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
-    }
-    
-    // O(log(min(m, n)))
-    while (i1_left  < = i1_right) {
-        i1 = i1_left + (i1_right - i1_left) / 2;
-        i2 = (nums1Size + nums2Size + 1) / 2 - i1;
-        if (i1 > 0 && nums1[i1 - 1] > nums2[i2]) {
-            i1_right = i1 - 1;
-        } else if (i1  <  nums1Size && nums1[i1] < nums2[i2 - 1]) {
-            i1_left = i1 + 1;
-        } else {
-            // found it!
-            if (i1 == 0) d = nums2[i2 - 1];
-            else if (i2 == 0) d = nums1[i1 - 1];
-            else if (nums1[i1 - 1] > nums2[i2 - 1]) d = nums1[i1 - 1];
-            else d = nums2[i2 - 1];
-            if (((nums1Size + nums2Size) % 2) == 0) {
-                if (i2 == nums2Size) {
-                    d = (d + nums1[i1]) / 2;
-                } else if (i1 == nums1Size) {
-                    d = (d + nums2[i2]) / 2;
-                } else if (nums1[i1]  <  nums2[i2]) {
-                    d = (d + nums1[i1]) / 2;
-                } else {
-                    d = (d + nums2[i2]) / 2;
-                }
-            }
-            break;
-        }
-    }
-#else // binary search 40+ms
-    int p1, p2;
-    
-    p1 =  (nums1Size + nums2Size - 1) / 2;
-    p2 = ((nums1Size + nums2Size) % 2) ? 0 : 1;
-    
-    if (nums2Size == 0) {
-        d = nums1[p1];
-        if (p2) {
-            d += nums1[p1 + 1];
-            d /= 2;
-        }
-        return d;
-    }
-    
-    if (nums1Size == 0) {
-        d = nums2[p1];
-        if (p2) {
-            d += nums2[p1 + 1];
-            d /= 2;
-        }
-        return d;
-    }
 
-    if (nums2[0]  <  nums1[0]) return bs2(nums2, nums2Size, nums1, nums1Size, p1, p2);
-    return bs2(nums1, nums1Size, nums2, nums2Size, p1, p2);
-#endif
-    
-    return d;
-}
+        // Sort the merged array.
+        sort(merged.begin(), merged.end());
+
+        // Calculate the total number of elements in the merged array.
+        int total = merged.size();
+
+        if (total % 2 == 1) {
+            // If the total number of elements is odd, return the middle element as the median.
+            return static_cast<double>(merged[total / 2]);
+        } else {
+            // If the total number of elements is even, calculate the average of the two middle elements as the median.
+            int middle1 = merged[total / 2 - 1];
+            int middle2 = merged[total / 2];
+            return (static_cast<double>(middle1) + static_cast<double>(middle2)) / 2.0;
+        }
+    }
+};
