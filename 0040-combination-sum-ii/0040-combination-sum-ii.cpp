6171d4da7d95ecbbb5464d58160cc35edbc186ca
Shrutijaiswal1989 <153531904+Shrutijaiswal1989@users.noreply.github.com>
2025-09-12 15:16:44 +0530

    Time: 3 ms (40.05%), Space: 14 MB (82.69%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,24 @@
 class Solution {
- public:
-  vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-    vector<vector<int>> ans;
-    ranges::sort(candidates);
-    dfs(candidates, 0, target, {}, ans);
-    return ans;
-  }
-
- private:
-  void dfs(const vector<int>& A, int s, int target, vector<int>&& path,
-           vector<vector<int>>& ans) {
-    if (target < 0)
-      return;
-    if (target == 0) {
-      ans.push_back(path);
-      return;
+public:
+  void findcombination(int ind,int target,vector<int>& arr,vector<vector<int>>& ans, vector<int>& ds){
+    if(target==0){
+        ans.push_back(ds);
+        return;
     }
-
-    for (int i = s; i < A.size(); ++i) {
-      if (i > s && A[i] == A[i - 1])
-        continue;
-      path.push_back(A[i]);
-      dfs(A, i + 1, target - A[i], move(path), ans);
-      path.pop_back();
+    for(int i=ind;i<arr.size();i++){
+        if(i>ind && arr[i]==arr[i-1]) continue;
+        if(arr[i]>target) break;
+        ds.push_back(arr[i]);
+        findcombination(i+1,target-arr[i],arr,ans,ds);
+        ds.pop_back();
     }
   }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findcombination(0,target,candidates,ans,ds);
+        return ans;
+        
+    }
 };
